228cc71e101ff3e5406b540319a982f8d9340afc
KozelCzech <adam.kopi@centrum.cz>
2021-08-27 14:51:39 +0200

    third commit

--- ItemPickUpNotification/Data/ShowItem.cs
+++ ItemPickUpNotification/Data/ShowItem.cs
@@ -5,10 +5,15 @@ using UnityEngine.UI;
 
 public class ShowItem : MonoBehaviour
 {
+    struct itemInfo
+    {
+        public ItemDefinition item;
+        public int amount;
+        public float time;
+    }
 
 
-
-    private InventoryItem[] slot = EmptyArray<InventoryItem>.Value;
+    
     //private Inventory m_items = new Inventory();
 
     //float usedSlotAmount, nextEmptySlot;
@@ -16,86 +21,159 @@ public class ShowItem : MonoBehaviour
     public Image itemSprite;
     public Text itemName;
 
-    public ItemDefinition item;
+    public GameObject container;
 
-    public bool isShown;
 
-    public float time = 5f;
+    public bool timerRunning = false;
+
+    
     public float remainingTime;
-    
-    
-    //void Start()
-    //{
-    //    //usedSlotAmount = m_items.SlotCount;
-    //    //nextEmptySlot = m_items.SlotCount + 1;
-    //    ShowItemOnScreen();
-    //    itemSprite = null;
-    //    itemName = null;
-    //    //DepositLocation.DepositMined
-    //    remainingTime = time;
-    //}
-    //void Update()
-    //{
-    //    //Amount of slots used changes
-    //    //if (usedSlotAmount != nextEmptySlot - 1)
-    //    //{
-    //    //    isShown = true;
-            
-    //    //}
+    public int slots;
 
-    //    //if true displays item on screen
-    //    if(isShown)
-    //    {
-    //        this.gameObject.SetActive(true);
-    //        ShowItemOnScreen();
-    //        //timer until text on screen disappears
-    //        if (remainingTime > 0)
-    //        {
-    //            remainingTime = remainingTime - Time.deltaTime;
-    //        }
-    //        if(remainingTime == 0)
-    //        {
-    //            isShown = false;
-    //            remainingTime = time;
-    //            this.gameObject.SetActive(false);
-    //        }
-    //    }
+    
+
+    Dictionary<ItemDefinition, int> m_items = new Dictionary<ItemDefinition, int>();
+    Dictionary<ItemDefinition, int> m_previousItems = new Dictionary<ItemDefinition, int>();
+    List<itemInfo> m_addedItems = new List<itemInfo>();
+    Inventory m_playerInventory;
+    int m_version = -1;
+
+    bool UpdateItems()
+    {
+        if (Player.Local.TryCacheComponentSafe(ref m_playerInventory) && m_version != m_playerInventory.Version)
+        {
+            //m_addedItems.Clear();
+            m_items.Clear();
+            m_version = m_playerInventory.Version;
+            foreach (var slot in m_playerInventory.Slots)
+            {
+                if (slot.IsValid)
+                {
+                    m_items[slot.Item] = m_items.GetValueOrDefault(slot.Item) + slot.Amount;
+                }
+            }
+
+            foreach (var item in m_items)
+            {
+                if (!m_previousItems.TryGetValue(item.Key, out int amount) || amount < item.Value)
+                {
+                    // This item was added
+                    itemInfo info;
+                    info.item = item.Key;
+                    info.amount = item.Value - amount;
+                    info.time = 3f;
+                    m_addedItems.Add(info);
+                }
+            }
+            m_previousItems.Clear();
+
+            // Swap previous items and items
+            var tmp = m_previousItems;
+            m_previousItems = m_items;
+            m_items = tmp;
+            return true;
+        }
+        return false;
+    }
+    
+    void Update()
+    {
+        //    //Amount of slots used changes
+        //    //if (usedSlotAmount != nextEmptySlot - 1)
+        //    //{
+        //    //    isShown = true;
+
+        //    //}
+
+
+        int amountOfItem = 0;
+        
+        if (UpdateItems())
+        {
+            StartTimer();
+            
+            TemplateCollection.Ensure(container, container.transform.GetChild(0).gameObject, 5);
+            foreach (var item in m_addedItems)
+            {
+                var img = container.transform.GetChild(amountOfItem).GetComponentInChildren<Image>();
+                img.sprite = item.item.Icon;
+                var text = container.transform.GetChild(amountOfItem).GetComponentInChildren<TMPro.TMP_Text>();
+                text.text = item.item.Name + " x" + item.amount;
+                Debug.Log(item.item.Name + "     " + item.amount);
+                amountOfItem++;
+                remainingTime = item.time;
+                if(amountOfItem == 5)
+                {
+                    amountOfItem = 0;
+                }
+            }
+            //for(int i = -1; i < amountOfItem; i++)
+            //{
+            //    var img = container.transform.GetChild(i).GetComponentInChildren<Image>();
+            //    img.sprite = item.item.Icon;
+            //    var text = container.transform.GetChild(i).GetComponentInChildren<TMPro.TMP_Text>();
+            //    text.text = item.item.Name + " " + item.amount;
+            //}
+            
+        }
+        if(timerRunning)
+        {
+            
+            if(remainingTime > 0)
+            {
+                remainingTime = remainingTime - Time.deltaTime;
+                Debug.Log(remainingTime);
+            }
+            else
+            {
+                Debug.Log("Timer ran out!");
+                //for(int i = -1; i < amountOfItem; i++)
+                //{
+                //    var itembox = container.transform.GetChild(i).gameObject;
+                //    itembox.SetActive(false);
+                //}
+                
+                m_addedItems.Clear();
+                timerRunning = false;
+            }
+
+        }
+        
 
         
-    //}
-    
-    //gets item from inventory
-    //private void GetItem(InventoryItem slot)
+
+
+    }
+    private void StartTimer()
+    {
+        timerRunning = true;
+
+    }
+
+
+//shows the sprite and name of item on screen
+
+    //private void OnEnable()
     //{
-    //    item = slot.Item;
+    //    DepositLocation.DepositMined += OnDepositMined;
     //}
-    
 
+    //private void OnDisable()
+    //{
+    //    DepositLocation.DepositMined -= OnDepositMined;
+    //}
 
-    //shows the sprite and name of item on screen
-    private void ShowItemOnScreen()
-    {
-        itemName.text = item.Name;
-        itemSprite.sprite = item.Icon;
-    }
-    private void OnEnable()
-    {
-        DepositLocation.DepositMined += OnDepositMined;
-    }
+    //private void OnDepositMined(GameObject minedBy, ItemDefinition minedItem, float mineAmount)
+    //{
+    //    // Tohle se zavolá když hráč něco natěží
+    //    container.SetActive(true);
+    //    remainingTime = time;
+    //    isShown = true;
+    //    item = minedItem;
+    //    Debug.Log("object mined");
+        
 
-    private void OnDisable()
-    {
-        DepositLocation.DepositMined -= OnDepositMined;
-    }
-
-    private void OnDepositMined(GameObject minedBy, ItemDefinition minedItem, float mineAmount)
-    {
-        // Tohle se zavolá když hráč něco natěží
-        isShown = true;
-        item = minedItem;
-        Debug.Log("object mined");
-
-    }
+    //}
 
 
 
